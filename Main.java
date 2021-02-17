99311b12556bfe05eada3dbb6932a841c5277827
Ethan Jeffcoat <ethan.jeffcoat@patelhs.org>
2021-02-17 16:34:49 +0000

    I added a recursion method.

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 class Main {
   public static void main(String[] args) {
-   sayHiHowAreYou(5);
-   countBackwards(80);
+    sayHiHowAreYou(5);
   }
 
   public static void sayHiHowAreYou(int n) {
@@ -11,22 +10,6 @@ class Main {
       System.out.println("Hi!, How are you?");
       n--;
       sayHiHowAreYou(n);
-    }
-  }
-  public static void countBackwards(int j){
-   if (j == 0){
-   System.out.println("Done!");
-   }else{
-   System.out.println(j);
-   j--;
-   countBackwards(j);
    }
-  
-  
   }
-
-
-
-
 }
-
